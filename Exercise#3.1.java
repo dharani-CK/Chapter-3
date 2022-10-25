f10bd25b92102a512b2f74c0160b05b5a0453e87
dharani-CK <113404041+dharani-CK@users.noreply.github.com>
2022-10-25 17:27:47 -0500

    Create Exercise#3.1.java

---
+++ Exercise#3.1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; //importing a scanner class
+
+public class QuadraticEquation
+{
+	public static void main(String[] args) {
+    
+	    Scanner input = new Scanner(System.in); //creating a new scanner class object
+	    
+	    //prompt the user to enter a, b and c as (ax^2 + bx + c = 0)
+	    System.out.print("Enter a, b, c: ");
+	    double a = input.nextDouble();
+	    double b = input.nextDouble();
+	    double c = input.nextDouble();
+		  
+		  //calculating the roots of the quadratic equation
+		  double discriminant = ((b * b) - (4 * a * c)); //assigning the value for discriminant
+		  double d = Math.pow(discriminant, 0.5); //square root of (b^2-4ac)
+		  double root1 = ((-1 * b) + d) / (2 * a);
+		  double root2 = ((-1 * b) - d) / (2 * a);
+		
+		  //checking whether the equation has real roots or not
+		  if(discriminant > 0){
+		      System.out.println("The equation has two roots " + root1 + " and " + root2 + ".");
+		  }
+		  else if(discriminant == 0){
+		      System.out.println("The equation has one root " + root1 + ".");
+		  }
+		  else if(discriminant < 0){
+		      System.out.println("The equation has no real roots");
+		  }
+		
+    }
+}
