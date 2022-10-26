a3555362c1b861f7ccb7c93a133020e7b6489bd2
dharani-CK <113404041+dharani-CK@users.noreply.github.com>
2022-10-25 19:11:25 -0500

    Update Exercise#3.2.java

--- Exercise#3.2.java
+++ Exercise#3.2.java
@@ -1,1 +1,35 @@
+/*
+This program generates three random numbers and prompts the user to enter the sum of these three random integers.
+Then, it checks whether the input is same as the sum and tells the user
+*/
+
+import java.util.Scanner; //importing a new scanner class
+
+public class AdditionQuiz
+{
+	public static void main(String[] args) {
+    
+	    Scanner input = new Scanner(System.in); //creating a new scanner class object
+	    
+	    //generating three random numbers
+	    int number1 = (int)(Math.random() * 10);
+	    int number2 = (int)(Math.random() * 10);
+	    int number3 = (int)(Math.random() * 10);
+	    
+	    //creating a new variable to add the three random numbers
+	    int sum = number1 + number2 + number3; 
+		
+		  //prompt the user to enter the sum of the numbers
+		  System.out.print("Enter " + number1 + " + " +  number2 + " + " + number3 + ": ");
+		  int answer = input.nextInt();
+		
+		  //check if answer = user's input
+		  if(answer == sum){
+		      System.out.println("That is correct. Keep it up!");
+		  }
+		  else{
+		      System.out.println("The answer is " + sum + ". Better luck next time!");
+		   }
+    }
+}
 
