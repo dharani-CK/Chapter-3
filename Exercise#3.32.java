cd02cf32a26a8ada86b14fae854976f392eb0a3a
dharanikatta <113404041+dharani-CK@users.noreply.github.com>
2022-11-08 17:45:25 -0600

    Create Exercise#3.32.java

---
+++ Exercise#3.32.java
@@ -0,0 +1,43 @@
+/*
+Given a directed line from point p0(x0, y0) to p1(x1, 
+y1), you can use the following condition to decide whether a point p2(x2, y2) is 
+on the left of the line, on the right, or on the same line.
+
+SAMPLE RUN:
+Enter three points for p0, p1, and p2: 4.4 2 6.5 9.5 −5 4
+p2 is on the left side of the line
+*/
+
+import java.util.Scanner; // importing scanner class
+
+public class PointPosition
+{
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in); // creating a new scanner class object
+	    
+	    // prompt the user to enter three points
+	    System.out.print("Enter three points for p0, p1, and p2: ");
+	    double x0 = input.nextDouble();
+	    double y0 = input.nextDouble();
+	    double x1 = input.nextDouble();
+	    double y1 = input.nextDouble();
+	    double x2 = input.nextDouble();
+	    double y2 = input.nextDouble();
+	    
+	    double distance = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+	    
+	    // check the point of position p2
+	    if (distance > 0){
+	        System.out.println("p2 is on the left side of the line");
+	    }
+	    else if (distance == 0){
+	        System.out.println("p2 is on the same line");
+	    }
+		else if (distance < 0){
+		    System.out.println("p2 is on the right side of the line");
+		}
+		
+		
+	}
+}
